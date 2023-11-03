68f6e22c69009eb17210ab7e4b9a46b809e99922
Sakif-Hridoy <sakifworkspace@gmail.com>
2023-11-04 01:04:40 +0600

    header add

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,11 @@
 import './App.css'
+import Header from './components/Header/Header'
 
 function App() {
 
   return (
     <>
-      
-      <h1 className='text-6xl bg-red-300'>Knowledge Cafe</h1>
-      
+      <Header></Header>
     </>
   )
 }
