325049a93f8f6328c45759f6aba4d7dea370e7c8
Sakif-Hridoy <sakifworkspace@gmail.com>
2023-11-04 12:37:56 +0600

    add blogs.json

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,7 +1,7 @@
 const Header = () => {
     return (
         <div>
-            <h1 className='text-4xl font-bold'>Knowledge Cafe</h1>
+            <h1 className='text-2xl font-bold'>Knowledge Cafe</h1>
         </div>
     );
 };
