8ddc482a3f68e87dd6b968882e096836b435335a
Sakif-Hridoy <sakifworkspace@gmail.com>
2023-11-04 22:34:37 +0600

    modify and add new blogs.json

--- src/components/Blog/Blog.jsx
+++ src/components/Blog/Blog.jsx
@@ -2,29 +2,29 @@ import PropTypes from 'prop-types';
 const Blog = ({blog}) => {
     // console.log(blog)
 
-    const{Title,Cover,Author,Author_img,Posted_date,Reading_time,Hashtags} = blog;
+    const{title,cover,author,author_image,posted_date,reading_time,hashtags} = blog;
     return (
         <div>
-            <img src={Cover} alt={`cover picture of the ${Title}`} />
+            <img src={cover} alt={`cover picture of the ${title}`} />
             <div className='flex justify-between'>
                 <div className='flex'>
-                <img className='w-14' src={Author_img} alt="" />
+                <img className='w-14' src={author_image} alt="" />
                 <div className='ml-4'>
-                    <h3 className='text-2xl'>{Author}</h3>
-                    <p>{Posted_date}</p>
+                    <h3 className='text-2xl'>{author}</h3>
+                    <p>{posted_date}</p>
                 </div>
 
                 </div>
 
                 <div>
-                    <span className=''>{Reading_time}12 min read</span>
+                    <span className=''>{reading_time} min read</span>
 
                 </div>
             </div>
-            <h2 className='text-4xl'>{Title}</h2>
+            <h2 className='text-4xl'>{title}</h2>
             <p>
                 {
-                    Hashtags.map(hash=><span key={blog.ID}><a href="">{hash}</a></span>)
+                    hashtags.map(hash=><span key={blog.ID}><a href="">#{hash}</a></span>)
                 }
             </p>
         </div>
