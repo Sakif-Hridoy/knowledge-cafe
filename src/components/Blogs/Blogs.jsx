8f14715dbe2d309e8c795a5a9332a3209401d970
Sakif-Hridoy <sakifworkspace@gmail.com>
2023-11-04 15:50:34 +0600

    made 3 components

--- src/components/Blogs/Blogs.jsx
+++ src/components/Blogs/Blogs.jsx
@@ -1,7 +1,19 @@
+import { useEffect } from "react";
+import { useState } from "react";
+
 const Blogs = () => {
+
+    const [blogs,setBlogs] = useState([]);
+
+    useEffect(()=>{
+        fetch('blogs.json')
+        .then(res=>res.json())
+        .then(data=>setBlogs(data))
+    },[])
+
     return (
-        <div>
-            <h3>Blogs</h3>
+        <div className="md:w-2/3">
+            <h1 className="text-4xl">Blogs: {blogs.length}</h1>
         </div>
     );
 };
